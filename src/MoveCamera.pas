147517df9df3651eee25d29d966cfc94b0163bb1
PaulTsevas <paul.tsevas@hotmail.com>
2012-06-08 20:47:05 +1000

    1st attempt at smooth camera

--- src/MoveCamera.pas
+++ src/MoveCamera.pas
@@ -17,7 +17,8 @@ begin
 	if (KeyDown(vk_a) and KeyDown(vk_w)) //<^
 	and ((CameraX() > map0X) and (CameraY() > map0Y)) then
 	begin
-		MoveCameraBy(-SQUARE_SIZE ,-SQUARE_SIZE );
+		//MoveCameraBy(-SQUARE_SIZE ,-SQUARE_SIZE );
+		MoveCameraBy(-1 ,-1 );
 		
 		topX -= 1;
 		topY -= 1;
@@ -29,7 +30,8 @@ begin
 	and (CameraY() > map0Y)) 
 	and (CameraX() < (MAP_SIZE * SQUARE_SIZE - ScreenWidth())) then
 	begin
-		MoveCameraBy(+SQUARE_SIZE ,-SQUARE_SIZE );
+		//MoveCameraBy(+SQUARE_SIZE ,-SQUARE_SIZE );
+		MoveCameraBy(+1 ,-1);
 		
 		topX += 1;
 		topY -= 1;
@@ -42,7 +44,8 @@ begin
 	and (CameraY() < (MAP_SIZE * SQUARE_SIZE - ScreenHeight())) 
 	and (CameraX() < (MAP_SIZE * SQUARE_SIZE - ScreenWidth())) then
 	begin
-		MoveCameraBy(+SQUARE_SIZE ,+SQUARE_SIZE );
+		//MoveCameraBy(+SQUARE_SIZE ,+SQUARE_SIZE );
+		MoveCameraBy(+1 ,+1);
 		
 		topX += 1;
 		topY += 1;
@@ -54,7 +57,8 @@ begin
 	and (CameraY() < (map0Y + (SQUARE_SIZE * (MAP_SIZE div 2 + 2) - ScreenHeight())))) 
 	and (CameraY() < (MAP_SIZE * SQUARE_SIZE - ScreenHeight())) then
 	begin
-		MoveCameraBy(-SQUARE_SIZE ,+SQUARE_SIZE );
+		//MoveCameraBy(-SQUARE_SIZE ,+SQUARE_SIZE );
+		MoveCameraBy(-1 ,+1);
 		
 		topX -= 1;
 		topY += 1;
@@ -63,7 +67,8 @@ begin
 		
 	end else if KeyDown(vk_w) and (CameraY() > map0Y) then //^
 	begin
-		MoveCameraBy(0 ,-SQUARE_SIZE );
+		//MoveCameraBy(0 ,-SQUARE_SIZE );
+		MoveCameraBy(0 ,-1);
 		
 		topY -= 1;
 		
@@ -73,7 +78,8 @@ begin
 	and (CameraY() < (map0Y + (SQUARE_SIZE * (MAP_SIZE div 2 + 2) - ScreenHeight()))) 
 	and (CameraY() < (MAP_SIZE * SQUARE_SIZE - ScreenHeight())) then 
 	begin
-		MoveCameraBy(0 ,+SQUARE_SIZE );
+		//MoveCameraBy(0 ,+SQUARE_SIZE );
+		MoveCameraBy(0 ,+1);
 		
 		topY += 1;
 		
@@ -81,7 +87,8 @@ begin
 		
 	end else if KeyDown(vk_a) and (CameraX() > map0X) then //<
 	begin
-		MoveCameraBy(-SQUARE_SIZE ,0 );
+		//MoveCameraBy(-SQUARE_SIZE ,0 );
+		MoveCameraBy(-1 ,0);
 		
 		topX -= 1;
 		
@@ -91,12 +98,14 @@ begin
 	and (CameraX() < (map0X + (SQUARE_SIZE * (MAP_SIZE div 2 + 8) - ScreenWidth())))
 	and (CameraX() < (MAP_SIZE * SQUARE_SIZE - ScreenWidth())) then 
 	begin
-		MoveCameraBy(+SQUARE_SIZE ,0 );
+		//MoveCameraBy(+SQUARE_SIZE ,0 );
+		MoveCameraBy(+1 ,0);
 		
 		topX += 1;
 		
 		//Delay(25);
 	end;
+	WriteLn(topX, ', ', topY, '. ', player.xLocation, ', ', player.yLocation);
 end;
 
 end.
